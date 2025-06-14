90319a3b203862d28fcfd64cbc31a67e0629218f
Claudia <yaneths2@hotmail.com>
2025-06-13 20:51:21 -0500

    modificar el test_model.py para que lea dinamicamente el modelo

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,19 +4,30 @@ import pytest
 from app import create_app
 from utils.s3_utils import download_from_s3
 
-load_dotenv()  # ✅ Cargar variables del archivo .env
+# Cargar variables de entorno desde .env o GitHub Actions
+load_dotenv()
 
+# Configuración de variables
 S3_BUCKET = os.getenv("S3_BUCKET_NAME")
 IMAGE_KEY = "Datos-prueba/Pug_600.jpg"
+MODEL_KEY = os.getenv("S3_MODEL_KEY", "densenet121_Opset17.onnx")
 LOCAL_IMG_PATH = f"tests/{IMAGE_KEY}"
+LOCAL_MODEL_PATH = "onnx_models/densenet121_Opset17.onnx"
 
 @pytest.fixture(scope="module")
 def client():
-    os.makedirs("tests", exist_ok=True)
+    os.makedirs("tests/Datos-prueba", exist_ok=True)
+    os.makedirs("onnx_models", exist_ok=True)
 
+    # Descargar imagen de prueba si no existe
     if not os.path.exists(LOCAL_IMG_PATH):
         download_from_s3(S3_BUCKET, IMAGE_KEY, LOCAL_IMG_PATH)
 
+    # Descargar modelo ONNX si no existe
+    if not os.path.exists(LOCAL_MODEL_PATH):
+        download_from_s3(S3_BUCKET, MODEL_KEY, LOCAL_MODEL_PATH)
+
+    # Crear la app Flask
     app = create_app()
     app.testing = True
     return app.test_client()
